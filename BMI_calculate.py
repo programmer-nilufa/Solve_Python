48238a22d6ade7fa958c81209b9d445f57617526
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-02-21 19:30:01 +0600

    BMI_Calculate

---
+++ BMI_calculate.py
@@ -0,0 +1,9 @@
+# weight =float (input("Enter your Weight :" ))
+# height =float (input ("Enter your Height : "))
+# bmi = weight/height ** 2
+# print("Your BMI is",bmi)
+
+Weight = input("Enter your Weight (kg) : ")
+Height = input("Enter your Height (Mitter) : ")
+BMI = int(Weight)/float(Height) ** 2
+print("Your BMI is :", int(BMI))
