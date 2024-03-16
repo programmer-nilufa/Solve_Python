160cfa81ec77debf49b526c3ce4dfedeec08866f
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-03-16 14:15:34 +0600

    if

---
+++ if.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter b number:"))
+c=int(input("Enter c number:"))
+if a>b:
+    print("The biggest number is a")
+elif b>c:
+    print("The biggest number is b")
+else:
+    print("The biggest number is c")
+
