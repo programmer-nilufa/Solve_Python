038a79ac0058060ff51dccccab0d4970fba52cda
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-04-22 06:39:48 +0600

    set

---
+++ set.py
@@ -0,0 +1,7 @@
+set_1 = {2,'ritu','mehedi',78}
+set_2 = {'ritu','plabon',115}
+set_3 = {'nilufa',15,'plabon'}
+# print(set_1)
+# print(set_1.union(set_2,set_3)) #CUP
+# print(set_1|set_2) #CUP
+print(set_1.update(set_2))
