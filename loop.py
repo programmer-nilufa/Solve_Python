4901e36e89eae7094e9961962b57fab0c5e538ff
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-05-16 03:01:24 +0600

    loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+# name = ['nilufa','ritu','mehedi']
+# n = []
+# for i in name:
+#     n.append(i)
+#
+# print(n)
+
+num = [2,4,6,8]
+list = []
+for i in num:
+    s = i**2
+    list.append(s)
+print(list)
