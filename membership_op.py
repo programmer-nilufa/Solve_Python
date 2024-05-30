53bf3ea87e561ce1e209ef0b002b15ac7cad3814
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-05-31 01:38:43 +0600

    membership_op

---
+++ membership_op.py
@@ -0,0 +1,10 @@
+var = "Nilufa Yesmin Ritu"
+a = "Nil"
+b = "Sagor"
+c = "Ritu"
+d = "Min"
+print(f"{a} in {var} :{a in var}")
+print (a, "in", var, ":", a in var)
+print (b, "in", var, ":", b in var)
+print (c, "in", var, ":", c in var)
+print (d, "in", var, ":", d in var)
