d52601b2cc0f2c05d590c1ceb71ce41e51cc1f13
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-08-03 10:10:44 +0600

    series_sum

---
+++ series_sum.py
@@ -0,0 +1,18 @@
+# num = int(input("Enter number here:"))
+# sum = 0
+# for i in range(1,num+1):
+#     sum=sum+i
+#     print(i)
+# print("The summestion is:",sum)
+
+num = [i for i in range(10)]
+print(num)
+
+n =[]
+for i in range(10):
+    n.append(i)
+print(n)
+
+
+numbers = [num for num in range(1,22+1,2)]
+print(numbers)
