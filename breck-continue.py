ef6196bb3c21f341ec681397096fa02a7866a1fe
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-08-05 20:21:33 +0600

    breck-continue

---
+++ breck-continue.py
@@ -0,0 +1,18 @@
+# i = 1
+# while i<=10:
+#     if i == 5:
+#         break
+#     print(i)
+#     i+=1
+
+
+
+count = 0
+n = []
+num = [10, 20, 30, 40, 50]
+while count<5 :
+    print(num[count])
+    count+=1
+
+n.append(num)
+print(n)
