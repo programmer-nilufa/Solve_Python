47f18f47309a2790931f07efbb16c2b16234d68a
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-08-17 12:27:18 +0600

    practice

---
+++ practice.py
@@ -0,0 +1,45 @@
+# count_num = int(input('Enter the total number: '))
+# numbers = []
+# for i in range(count_num):
+#     value = int(input('Enter the value here: '))
+#     numbers.append(value)
+# print(numbers)
+
+# a = 10
+# b = 2
+# c = a//b
+# print(c)
+
+# if a%2 == 0:
+#     print('even')
+
+
+
+# count_numbers = int(input('Enter the number:'))
+# for i in range(1,count_numbers+1):
+#     print(i)
+# for j in range(1,i+1):
+#     j+=i
+# print('the print number is :',j)
+
+
+
+count_numbers = int(input('Enter the number:'))
+n = []
+for i in range(1,count_numbers+1):
+    n.append(i)
+    print(n)
+    for j in range(i):
+        n.append(j)
+        print(n)
+
+
+
+
+
+
+
+
+
+
+
