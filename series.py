ded2ed6760edae8495f95969f0a18e5e0ea0ded3
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-08-18 01:21:56 +0600

    series

---
+++ series.py
@@ -0,0 +1,52 @@
+# 1+2+3+4+..............+n
+
+n = int(input('Enter the number :'))
+i = 1
+sum = 0
+
+while i <= n:
+    sum = sum + i
+    print(i)
+    i+=1
+
+print('The value of sum =',sum)
+
+
+# 2+4+6+8+..........+n
+"""
+n = int(input('Enter the number: '))
+i = 2
+s = 0
+while i <= n-1 :
+    s += i
+    print(i)
+    i += 2
+
+print('The value of sum = ',s)
+"""
+
+# 2+4+6+8+..........+n
+"""
+n = int(input('Enter the number: '))
+i = 1
+s = 0
+while i <= n-1 :
+    s += i
+    print(i)
+    i += 2
+
+print('The value of sum = ',s)
+"""
+
+# 4+8+12+16+..........+n
+"""
+n = int(input('Enter the number: '))
+i = 4
+s = 0
+while i<=n:
+    s+=i
+    print(i)
+    i+=4
+
+print('The value of sum = ',s)
+"""
