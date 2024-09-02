aae3ce439c116d444c5a01867d996662531a9540
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-09-02 13:02:21 +0600

    t-s-l-d

---
+++ t-s-l-d.py
@@ -0,0 +1,19 @@
+#tuple
+numbers = (1,3 ,9)
+print(numbers[1])
+print(type(numbers))
+
+#set
+num = {2, 4, 8}
+print(num)
+print(type(num))
+
+#list
+numb = [4, 6, 7]
+print(numb)
+print(type(numb))
+
+#dictionary
+nub = {1:'Ritu', 2:'Mehedi',3:'Nilufa',4:{'a':'Nisu','b':'Nishan'}}
+print(nub[2])
+print(type(nub))
