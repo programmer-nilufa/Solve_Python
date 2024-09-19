bd9ecee56d8fbd25596a1e4206f2ba5dbedc87d6
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-09-19 21:49:56 +0600

    functiion

---
+++ functiion.py
@@ -0,0 +1,84 @@
+# def name():
+#     print('Hi my name is Ritu')
+#
+# name()
+
+# def sum(num1:int,num2:int):
+#     num3 = num1+num2
+#     return num3
+#
+# num1, num2 = 5,6
+# ans = sum(num1,num2)
+# print('Your summesation result is: ',ans)
+
+
+# def add(num1: int, num2: int) -> int:
+#     """Add two numbers"""
+#     num3 = num1 + num2
+#
+#     return num3
+#
+#
+# # Driver code
+# num1, num2 = 5, 15
+# ans = add(num1, num2)
+# print(f"The addition of {num1} and {num2} results {ans}.")
+
+
+
+# def data(name):
+#     print('Hello',name)
+#
+#
+# data(name=input('Enter your name: '))
+
+# def num(roll):
+#     # print('Roll',roll)
+#     return roll
+#
+# print('Your Roll number :',num(roll=int(input('Enter your roll: '))))
+
+
+
+# def num(n1:int,n2:int):
+#     n = n1 + n2
+#     return n
+# n1 = int(input('Enter first number: '))
+# n2 = int(input('Enter second number: '))
+# print(num())
+
+
+def sum(n1,n2):
+    n = n1+n2
+    return n
+
+n1 = int(input('Enter first number: '))
+n2 = int(input('Enter second number: '))
+tot = sum(n1,n2)
+print('The summestion is sum: ',tot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
