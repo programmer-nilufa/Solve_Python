bc62076ba3c75c6c11e47654a827fcf8c10038a6
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2024-09-26 22:35:34 +0600

    guess_game

---
+++ guess_game.py
@@ -0,0 +1,22 @@
+import random
+# user  = int(input('Your choice number: '))
+# co = [1,2,3,4,5,6]
+# com = random.choice(co)
+# print('Computer choice number: ',com)
+# if user >= 7:
+#     print('Invalid Number!')
+# elif user > com:
+#     print('You Win!')
+# else:
+#     print('You loss!')
+
+
+# random.seed(3)
+# print(random.randint(1, 1000))
+# random.seed(9)
+# print(random.randint(1, 1000))
+
+
+for i in range(10):
+    e = random.random()
+    print(e)
